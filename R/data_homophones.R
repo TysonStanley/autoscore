51218d846a84aa7cf22dcfbbf861d4c95b444cca
TysonStanley <t.barrett@aggiemail.usu.edu>
2018-07-29 22:09:54 -0600

    updated docs

---
+++ R/data_homophones.R
@@ -0,0 +1,11 @@
+#' Homophones
+#'
+#' A dataset containing homophones.
+#'
+#' @format A data frame with 479 rows and 2 variables:
+#' \describe{
+#'   \item{rowname}{The row number.}
+#'   \item{homophone}{The homophones (each line contains two or more words separated by commas).}
+#' }
+#' @source \url{http://homophonelist.com/homophones-list/}
+"homophones"
---
+++ R/data_misspell.R
@@ -0,0 +1,10 @@
+#' Common Misspellings List
+#'
+#' A dataset containing common misspellings in the english lanugage.
+#'
+#' @format A data frame with 4,278 rows and 2 variables:
+#' \describe{
+#'   \item{original}{The original, generally-accepted spelling}
+#'   \item{alternate}{The misspelling of the original word.}
+#' }
+"alternate_df_default"
