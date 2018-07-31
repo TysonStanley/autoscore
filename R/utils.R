9b50898ef445e42a920cbd16892353ed2322167c
Tyson S. Barrett <t.barrett88@gmail.com>
2018-07-31 13:58:56 -0600

    updated output

--- R/utils.R
+++ R/utils.R
@@ -307,18 +307,28 @@ format_output <- function(final_table, output, original_data) {
     dplyr::rename_all(tolower)
 
   if (isTRUE("human" %in% names(final_table))){
+
+    orig_d2 <- original_data %>%
+      dplyr::select(-id, -target, -response, -human)
+
     ft <- final_table %>%
       dplyr::select(human, count_target) %>%
       dplyr::mutate(equal = human == count_target)
     ft <- cbind(original_data$id, original_data$target, original_data$response,
-                ft) %>%
-      stats::setNames(c("id", "target", "response", "human", "robot", "equal"))
+                ft, orig_d2) %>%
+      stats::setNames(c("id", "target", "response", "human", "robot", "equal",
+                        names(orig_d2)))
   } else {
+
+    orig_d2 <- original_data %>%
+      dplyr::select(-id, -target, -response)
+
     ft <- final_table %>%
       dplyr::select(count_target)
     ft <- cbind(original_data$id, original_data$target, original_data$response,
-                ft) %>%
-      stats::setNames(c("id", "target", "response", "robot"))
+                ft, orig_d2) %>%
+      stats::setNames(c("id", "target", "response", "robot",
+                        names(orig_d2)))
   }
 
   if (output == "text"){
