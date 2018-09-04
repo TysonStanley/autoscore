7737359ac7a7a99327ca6e8fc87178cc66377dac
Tyson S. Barrett <t.barrett88@gmail.com>
2018-09-04 16:05:29 -0600

    tell me homophones is fixed...

--- R/data_homophones.R
+++ R/data_homophones.R
@@ -2,10 +2,10 @@
 #'
 #' A dataset containing homophones.
 #'
-#' @format A data frame with 479 rows and 2 variables:
+#' @format A data frame with 481 rows and 2 variables:
 #' \describe{
 #'   \item{rowname}{The row number.}
 #'   \item{homophone}{The homophones (each line contains two or more words separated by commas).}
 #' }
 #' @source \url{http://homophonelist.com/homophones-list/}
-"phones"
+"homophones"
